27af38c4594a85b266c36e4a0afe5f1a4a3eea7a
Abi Karunaratne <Szask1a@users.noreply.github.com>
2022-09-26 00:00:10 +0530

    done

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# Canny Edge detection
+canny = cv.GaussianBlur(grayscale, (1, 1), 0)
+canny = cv.Canny(canny, 400, 410)
+cv.imwrite("images/" + "Edges" + ".png", canny)
+
+horizontal, vertical = detect_lines(image, title='Hough Lines', minLinLength=20, threshold=50, maxLineGap=4, display=True, write=True)
+
 # Inverted Binary Image
 inverted_binary = invert_area(grayscale, x, y, w, h, display=True)
 cv.imwrite("images/" + "Inverted Binary" + ".png", inverted_binary)
