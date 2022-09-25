01e4a5de99177572adb469e290fd4aec91c58bdf
Chandima Vishwajith <51170497+chandimavishwajith@users.noreply.github.com>
2022-09-25 22:45:45 +0530

    Delete attendence_manager/text directory

--- attendence_manager/text/main.py
+++
@@ -1,6 +0,0 @@
-# Inverted Binary Image
-inverted_binary = invert_area(grayscale, x, y, w, h, display=True)
-cv.imwrite("images/" + "Inverted Binary" + ".png", inverted_binary)
-
-output = table_detection(src)
-print(output)
