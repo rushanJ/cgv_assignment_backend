50726c2a7ba61bd3eede2c6528893205f576274b
PrasadSulochana <prasadsulochana10@gmail.com>
2022-09-26 00:11:50 +0530

    Conflits fix

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,15 @@
+import pytesseract
+import cv2 as cv
+from ROI_selection import detect_lines, get_ROI
+import numpy as np
+
+def get_grayscale(image):
+    return cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+def get_binary(image):
+    (thresh, blackAndWhiteImage) = cv.threshold(image, 100, 255, cv.THRESH_BINARY)
+    return blackAndWhiteImage
+
 def invert_area(image, x, y, w, h, display=False):
     ones = np.copy(image)
     ones = 1
